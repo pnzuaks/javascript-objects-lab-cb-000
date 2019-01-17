333c12662ad2ff8a16ef6e208afc77cbd9873292
Patricia Nykaza <pnykaza@calstatela.edu>
2019-01-17 20:50:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 var recipes = {'key':'value'}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({},object, {[key]: value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign(object, {[key]: value})
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newRecipe = Object.assign({}, object)
+  newRecipe
+  delete newRecipe[key]
+  return newRecipe
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
+}
