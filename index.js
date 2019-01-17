e810530ab48ca2ce2320eccdb7d22f8bfed61101
Patricia Nykaza <pnykaza@calstatela.edu>
2019-01-17 20:46:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return Object.assign(object, {[key]: value})
 }
 
-function destructivelyDeleteFromObjectByKey(object, key) {
+function deleteFromObjectByKey(object, key) {
   delete object[key]
   return object
 }
