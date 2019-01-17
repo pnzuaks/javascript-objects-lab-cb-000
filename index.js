348366f57a5e0cc744495fa9e0f1703912b28065
Patricia Nykaza <pnykaza@calstatela.edu>
2019-01-17 20:42:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ var recipes = {'key':'value'}
 
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({},object, {[key]: value})
+}
 
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({},object, {[key]: value})
 }
